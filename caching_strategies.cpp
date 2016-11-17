cd923627f159cf7649ce0c0ab6dcd25329c3ecea
Kartikay Gautam <kgautam@hawk.iit.edu>
2016-11-17 12:59:22 -0600

    block comments

---
+++ caching_strategies.cpp
@@ -0,0 +1,138 @@
+// ***-------------------------***-------------------------*** //
+
+// ***-------------------------***-------------------------*** //
+
+/*
+
+Assuming the following kind of structures
+to define the buffer class and the page
+class : -
+
+struct BufferPool
+{
+  int numPages; // the num of pages currently
+                   residing in the cache.
+
+  void *data; // represents the array of pages,
+                 which represents the data inside
+                 the cache.
+
+  size_t capacity; // the total capacity of the cache
+}
+
+struct Page
+{
+  char *pagedata;
+  int pagenum; default value = -1 => empty_page
+  int dirtybit; default value = -1 => not_dirty
+  int ranking; default value = INT_MAX
+  int readcount; default value = 0
+}
+
+ Note that the default values are only assumptions
+ and just a way of representing the data when we will
+ first initialize the buffer.
+
+ INT_MAX = 1000 ; basically need some large value.
+                  this will be used later to define
+                  'ranking' and thereafter guide our
+                  replacement policy, as we shall see.
+
+*/
+
+// ***-------------------------***-------------------------*** //
+
+// ***-------------------------***-------------------------*** //
+
+
+/*
+ *    LEAST RECENTLY USED -  Cache Replacement Policy
+ *
+ * Definition - replaces the least recently used page
+ *
+ * Paramters - (i) a buffer handler, defined by the struct
+ *             given in the description above, which
+ *             represents our cache here.
+ *
+ *             (ii) the fresh page which needs to be added.
+ *             The page has the form defined in the struct
+ *             above
+ *
+ * Description - Basically we read in the data stored inside
+ *               the cache/buffer into a data/pages pointer.
+ *
+ *               Then we iterate over each of these pages to
+ *               search for the page with the minimum rank.
+ *
+ *               If the minimum ranking page has its dirty bit
+ *               set, i.e., it has been updated after it was
+ *               read in from the backing store to the cache,
+ *               Then, we have to write the data back to the
+ *               main storage to persist the changes.
+ *
+ *               Then we replace the data at this index, within
+ *               our data array, with our fresh data.
+ *
+ *               The ranking of a newly used page is updated to
+ *               be INT_MAX. To represent the most recently used
+ *               page.
+ *
+ *               The rankings of all the other pages is updated
+ *               in the decrease ranking method, wherein we
+ *               reduce the ranking of each by subtracting 1 from
+ *               their existing ranking.
+ *               
+ *               The last two things are updated in a seperate
+ *               function called decreaseRankingForPages(...)
+ */
+
+
+void lru (BufferPool *const bm, Page *freshpage)
+{
+  Page *pages = (page *)bm->data;
+  int i;
+  int eviction_index = 0;
+  int minimum_rank = INT_MAX;
+
+  //search for min rank.
+  for (i = 0; i<bm->numPages; i++)
+  {
+    if (pages[i].ranking < minimum_rank)
+    {
+      minimum_rank = pages[i].ranking;
+      eviction_index = i;
+    }
+  }
+
+  if(pages[eviction_index].dirtybit == DIRTY_FLAG)
+  {
+    // Write block to backing store
+    //write_function_prototype(bm, &pages[eviction_index]);
+  }
+  pages[eviction_index].pagedata = freshpage->pagedata;
+  pages[eviction_index].pagenum = freshpage->pagenum;
+  pages[eviction_index].dirtybit = freshpage->dirtybit;
+
+  decreaseRankingForPages(bm, eviction_index);
+}
+
+// ***-------------------------***-------------------------*** //
+
+void decreaseRankingForPages(BufferPool *const bm, int evictedpage_index)
+{
+  Page *pages = (Page *) bm->data;
+  int i;
+  for (i = 0; i<bm->numPages; i++)
+  {
+    if (i != evictedpage_index || pages[i].pagenum != -1)
+    {
+      pages[i].ranking = pages[i].ranking - 1;
+    }
+    if (i == evictedpage_pageindex)
+    {
+      pages[i].ranking = INT_MAX;
+    }
+  }
+}
+
+// ***-------------------------***-------------------------*** //
