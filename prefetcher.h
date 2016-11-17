25b4af0f3cf844487e3a4ead7be8d3d893c0602e
Zeeshan Khavas <zkhavas@hawk.iit.edu>
2016-11-17 10:55:15 -0600

    updated interface

---
+++ prefetcher.h
@@ -0,0 +1,27 @@
+#ifndef PREFETCHER
+#define PREFETCHER
+
+#include "buffer.h"
+#include <cstddef>
+#include <cstring>
+#include <stdexcept>
+
+class prefetcher
+{
+public:
+	typedef int pageNumber;
+
+	typedef struct pageHandler {
+		pageNumber pageNum;
+		char *data;	
+	} pageHandler;
+
+	prefetcher();
+
+	int prefetch(Buffer *const buff);
+
+	~prefetcher();
+	
+};
+
+#endif
