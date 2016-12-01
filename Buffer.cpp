10e9f329105cee3251e78b03f713140a019d6114
Zeeshan Khavas <zkhavas@hawk.iit.edu>
2016-11-30 19:12:52 -0600

    updated buffer functions

--- Buffer.cpp
+++ Buffer.cpp
@@ -2,20 +2,38 @@
 #include <stdlib.h>
 
 
-void Buffer::Buffer(const Buffer& buff) {
-	if(buff.capacity > 0 )
+void Buffer::Buffer(size_t capacity, size_t pagesize, ReplacementStrategy strat) {
+
+	if(capacity > 0 )
 	{
-		data = new char[buff.capacity];
+		capacity_ = capacity; 
+		pagesize_ = pagesize;
 
-		if(buff.size > 0)
-		  std::memcpy (data, buff.data, buff.size);
+		numPages = (capacity / pagesize);
+		data = new char[capacity_];
 
-		if(buff.lastposition >= 0)
-			lastposition = buff.lastposition;
+		Page *pages = malloc(sizeof(Page) * numPages);
 	}
-	else data_ = 0;
-
-	size = buff.size;
-	capacity = buff.capacity;
-	
 }
+
+// void Buffer::Buffer(const Buffer& buff) {
+// 	if(buff.capacity > 0 )
+// 	{
+// 		data = new char[buff.capacity];
+
+// 		if(buff.size > 0)
+// 		  std::memcpy (data, buff.data, buff.size);
+
+// 		// if(buff.lastposition >= 0)
+// 		// 	lastposition = buff.lastposition;
+// 	}
+// 	else data_ = 0;
+
+// 	size = buff.size;
+// 	capacity = buff.capacity;
+// 	strate
+
+
+// }
+
+void Buffer::append()
