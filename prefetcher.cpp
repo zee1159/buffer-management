28c354efcf694960c1fb4f56c21f0bce52428ea3
Zeeshan Khavas <zkhavas@hawk.iit.edu>
2016-11-30 21:01:41 -0600

    updated function to fetch data

--- prefetcher.cpp
+++ prefetcher.cpp
@@ -1,7 +1,7 @@
 #include <prefetcher.h>
 #include <stdlib.h>
 
-inline int prefetcher::prefetch(Buffer *const buff, FILE *fh, size_t size, size_t count){
+int prefetcher::prefetch(Buffer *const buff, FILE *fh, size_t size, size_t count){
 	int check, lastposition;
 	char *lastPageData;
 	bool checkData;
@@ -32,72 +32,10 @@ inline int prefetcher::prefetch(Buffer *const buff, FILE *fh, size_t size, size_
 		throw std::invalid_argument("Error reading from file!");
 	}
 
+	append(data, size);
+
 	//returning file handler pointer to the previous position
 	fseek(fh, previous_pos, SEEK_SET);
-
-	/*------------------------*/
-	/*----< PREVIOUS CODE>----*/
-	/*------------------------*/
-
-	//buffer is empty return error code (-1)
-	if(buff -> data == NULL) {
-		return -1;
-	}
-
-	// ------------- Get last read page --------------- //
-
-	// For which need to get the data inside the buffer
-	Page *pages = (page *)buff -> data
-
-	// index of last inserted page inside this pages array
-	// = last position, read that,
-	lastposition = buff -> lastposition;
-
-	// get the data at this index
-	lastPageData = pages[lastposition] -> pagedata;
-
-	// compare if void * data = lastpage data
-	checkData = (data == lastPageData) ? TRUE : FALSE;
-
-	// braches according to the checkdata result
-
-	// if checkdata = true,
-	// then fetch the next data from the backing store.
-	if(checkData){
-
-		// check buffer capacity
-		if (buff.checkCapacity()){
-
-			// fetch next data from the backing store,
-	 		// which we will store in newdata
-			buff.append(newDataa)
-		}
-		else{
-
-			// call cache manager
-			cache_manager cm;
-
-			cm.perfromReplacement(buff, &newData)
-
-		}
-	}
-	else{
-
-		// check buffer capacity
-		if (buff.checkCapacity()){
-
-			// fetch next data from the backing store,
-	 		// which we will store in newdata
-			buff.append(newDataa)
-		}
-		else{
-
-			// call cache manager
-			cache_manager cm;
-
-			cm.perfromReplacement(buff, &newData)
-
-		}
-	}
 	
+	return 0;
 }
