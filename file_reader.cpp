7a7d67c85e6183e2b278d4130536c6f58e3ff4c0
Zeeshan Khavas <zkhavas@hawk.iit.edu>
2016-11-30 23:25:44 -0600

    Added stub to check the caching mechanism

---
+++ file_reader.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <prefetcher.h>
+
+int main()
+{
+	/* code */
+	FILE *fh;
+	size_t size;
+	long count;
+	char *buff;
+	int status;
+
+	Buffer buffer;
+	buffer.Buffer(size_t 1000, size_t 10);
+
+	fh = fopen("helloworld.c", "rb");
+
+	//getting size
+	fseek(fh, 0, SEEK_END);
+	count = ftell(fh);
+	rewind(fh);
+
+	buff = (char*) malloc(sizeof(char) * count);
+
+	fread(buff, 1, 10, fh);
+	status = prefetch(buffer, fh, size, count);
+
+	printf("Status: %d\n", status);
+
+	return 0;
+}
